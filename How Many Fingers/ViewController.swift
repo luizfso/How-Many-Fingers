d8dee86ea3aa6bc0b1b3e7a5921fbf366506151b
Luiz Fernando Santiago <luizfso@gmail.com>
2015-10-06 13:56:33 -0300

    My class for guessing how many fingers

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func guess(sender: AnyObject) {
+        
+        let diceRoll = String(arc4random_uniform(6))
+        
+        print(diceRoll)
+        
+        if diceRoll == userGuessTextField.text{
+        
+            resultLabel.text = "You're right"
+        }  else if resultLabel.text!.isEmpty {
+            resultLabel.text = "please enter a number"
+        } else {
+            resultLabel.text = "Wrong! It was a " + diceRoll
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
